7f10bb277f3c1f7be572b39a61d50e50420ecfe3
Tiam Rietsch <NgnotchoueTiam@gmail.com>
2022-08-27 19:59:42 +0100

    commit

--- users/views.py
+++ users/views.py
@@ -1,12 +1,20 @@
-from calendar import c
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
 
 class SignupView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'users/signup.html'
     success_url = reverse_lazy('login')
-
-  
+    
+    def form_valid(self, form):
+        form.save()
+        new_user = authenticate(
+            username = form.cleaned_data["username"],
+            password = form.cleaned_data["password1"]
+        )
+        login(self.request, new_user)
+        return HttpResponseRedirect(reverse_lazy('cat_list'))
